0ac91dfe4f41782689691127e5f02e4673f18b62
joerlop <jj.erlich155@gmail.com>
2020-04-10 09:26:51 -0500

    Removed build from gitignore

---
+++ directory/build/precache-manifest.25fa8f7e6e711d09c0b712a63c562ed9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d3a2e553973fb9edf3a4070d3e41d7",
+    "url": "/directory.btcpayserver.org/index.html"
+  },
+  {
+    "revision": "ebbee202de0da9677c06",
+    "url": "/directory.btcpayserver.org/static/css/main.53db9320.chunk.css"
+  },
+  {
+    "revision": "ff4754e6378e31c890e2",
+    "url": "/directory.btcpayserver.org/static/js/2.37fe268a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/directory.btcpayserver.org/static/js/2.37fe268a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbee202de0da9677c06",
+    "url": "/directory.btcpayserver.org/static/js/main.370a4237.chunk.js"
+  },
+  {
+    "revision": "aaafed1ae1a724d03192",
+    "url": "/directory.btcpayserver.org/static/js/runtime-main.b16a841f.js"
+  },
+  {
+    "revision": "a51d790b3fd71b506e6766701e1e3bd1",
+    "url": "/directory.btcpayserver.org/static/media/btcpay-directory-logo-white.a51d790b.png"
+  },
+  {
+    "revision": "6e3359f9b05a554e80377d08e7ca1e03",
+    "url": "/directory.btcpayserver.org/static/media/btcpay-directory-logo.6e3359f9.svg"
+  },
+  {
+    "revision": "7fe50f25c11d11f5833b33d03a17fa04",
+    "url": "/directory.btcpayserver.org/static/media/moonFilled.7fe50f25.svg"
+  },
+  {
+    "revision": "eae3aacdfedd630b137d25a46201d928",
+    "url": "/directory.btcpayserver.org/static/media/sunFilled2.eae3aacd.svg"
+  }
+]);
