15928d9c3bf50b870981355071a92bd29a52d617
Dennis Reimann <mail@dennisreimann.de>
2020-04-11 18:34:38 +0200

    Set color mode in localStorage

--- directory/src/index.js
+++ directory/src/index.js
@@ -5,9 +5,14 @@ import App from "./App";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
+const COLOR_MODES = ["light", "dark"];
+const systemColorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+const userColorMode = window.localStorage.getItem('colorMode');
+const colorMode = COLOR_MODES.includes(userColorMode) ? userColorMode : systemColorMode;
+
 ReactDOM.render(
   <Router>
-    <App />
+    {<App colorMode={colorMode} />}
   </Router>,
   document.getElementById("root")
 );
