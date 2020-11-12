7e23a4f2bb579e4f525bc21c110d8a2465e603db
Daniel Leedan <d4n13l.l33d4n@gmail.com>
2020-11-12 10:13:49 +0800

    New entry: Elephant Chateau
    Closes #158

--- src/components/Directory/List.js
+++ src/components/Directory/List.js
@@ -1501,4 +1501,12 @@ export const list = [
     description:
       "Get Vegan Pizzas delivered to your doorstep by paying with BTCPay Server."
   },
+  {
+    name: "Elephant Chateau",
+    url: "https://elephantchateau.com/",
+    type: "merchants",
+    subType: "food",
+    description:
+      "Experience The World's Finest Teas & Spices (Free US Shipping + Bitcoin Accepted)."
+  },
 ];
