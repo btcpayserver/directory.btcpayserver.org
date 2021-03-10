3e1fc89e7a31d54d9dc282e0308660fc71162136
Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>
2021-03-10 11:44:36 +0100

    add onion
    Co-authored-by: Zaxounette <51208677+Zaxounette@users.noreply.github.com>

--- src/components/Directory/List.js
+++ src/components/Directory/List.js
@@ -38,7 +38,7 @@ export const list = [
     type: "merchants",
     subType: "education",
     description:
-      "The Agora Towards Liberty is a gathering place for peers to foster a holistic understanding of applied freedom strategies with tailor made education for your individual curiosity. Using the ancient Socratic method of truth discovery through reasoning and gathering with peers to discuss in depth any topic worth understanding."
+      "The Agora Towards Liberty is a gathering place for peers to foster a holistic understanding of applied freedom strategies with tailor made education for your individual curiosity. Using the ancient Socratic method of truth discovery through reasoning and gathering with peers to discuss in depth any topic worth understanding. Also reachable through Tor at http://2wp7zwpx6myz7kbdjuthmtccvs6p3tildwhrnm4zmv2fnizrfeo7hvyd.onion/"
   },
   {
     name: "Gab",
