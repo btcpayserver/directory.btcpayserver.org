e91b6724ee203f938a3c5eb4ef0eafb220f19ae3
Phaedrus Raznikov <praznikov@cerego.com>
2022-04-27 16:33:05 -0400

    Use id instead of index, refactor Loader conditional

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -53,27 +53,18 @@ function Directory(props) {
       <div className="list-container">
         {/* dirList is the filtered list of companies - it contains only the companies
             that match the current filter */}
-        {dirList.map((user, i) => (
+        {dirList.map((user) => (
           <LazyLoad
             height={0}
             once
-            key={i}
+            key={user.id}
             placeholder={
-              colorMode === "light" ? (
-                <Loader
-                  type="ThreeDots"
-                  color="#000000"
-                  height="15"
-                  width="30"
-                />
-              ) : (
-                <Loader
-                  type="ThreeDots"
-                  color="#ffffff"
-                  height="15"
-                  width="30"
-                />
-              )
+              <Loader
+                type="ThreeDots"
+                color={colorMode === "light" ? "#000000" : "#ffffff"}
+                height="15"
+                width="30"
+              />
             }
           >
             <DirectoryItem key={user.name} user={user} />
