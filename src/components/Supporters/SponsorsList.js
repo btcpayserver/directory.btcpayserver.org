4fa9413c12f1f51d42eafa27b148f3cba1234a6c
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-09-24 21:02:16 +0530

    Close btcpayserver#88 Added supporter logos

---
+++ src/components/Supporters/SponsorsList.js
@@ -0,0 +1,65 @@
+import SpiralLogo from "./icons/spiral.svg";
+import Baillie from "./icons/bailliegifford.svg";
+import Strike from "./icons/strike.svg";
+import Hrf from "./icons/hrf.svg";
+import Pnxbet from "./icons/pnxbet.png";
+import Lunanode from "./icons/lunanode.svg";
+import Walletofsatoshi from "./icons/walletofsatoshi.svg";
+import Nomics from "./icons/nomics.svg";
+import Coincards from "./icons/coincards.svg";
+import Ivpn from "./icons/ivpn.svg";
+
+const SponsorList = [
+  {
+    name: "Spiral",
+    image: SpiralLogo,
+    url: "https://spiral.xyz/",
+  },
+  {
+    name: "Baillie Gifford",
+    image: Baillie,
+    url: "https://www.bailliegifford.com/",
+  },
+  {
+    name: "Strike",
+    image: Strike,
+    url: "https://strike.me/",
+  },
+  {
+    name: "HRF",
+    image: Hrf,
+    url: "https://hrf.org/",
+  },
+  {
+    name: "PNXBET",
+    image: Pnxbet,
+    url: "https://www.pnxbet.io/",
+  },
+  {
+    name: "LunaNode",
+    image: Lunanode,
+    url: "https://www.lunanode.com/",
+  },
+  {
+    name: "Wallet of Satoshi",
+    image: Walletofsatoshi,
+    url: "https://walletofsatoshi.com/",
+  },
+  {
+    name: "Nomics",
+    image: Nomics,
+    url: "https://nomics.com/",
+  },
+  {
+    name: "Coincards",
+    image: Coincards,
+    url: "https://coincards.com/",
+  },
+  {
+    name: "IVPN",
+    image: Ivpn,
+    url: "https://www.ivpn.net/",
+  },
+];
+
+export default SponsorList;
---
+++ src/components/Supporters/Supporter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import SponsorList from "./SponsorsList.js";
+import "./Supporter.scss";
+
+function Supporter() {
+  return (
+    <div>
+      <div className="heading">Foundation Supporters</div>
+      <div className="centered-container-logos space-x ">
+        {SponsorList.map((sponsor) => {
+            return (
+                <div className="logos">
+                  <a className="hover-underline" href={sponsor.url} >
+                    <img className="image" src={sponsor.image} alt={sponsor.name} />
+                    <div className="image-text">{sponsor.name}</div>
+                  </a>
+                </div>
+            );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Supporter;
