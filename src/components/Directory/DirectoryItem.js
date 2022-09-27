6c10792ba64ffe5cf0be130a1e22735aed93afb3
Vaibhav Garg <vaibhav.garg237@gmail.com>
2022-09-27 21:40:49 +0530

    Added onionUrl icon Close #199

--- src/components/Directory/DirectoryItem.js
+++ src/components/Directory/DirectoryItem.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./Directory.scss";
 import { icons } from "./Icons";
+import iconSprite from "../../images/icon-sprite.svg";
 
 function DirectoryItem(props) {
   const formattedTwitter = props.user.twitter ? props.user.twitter.slice(1) : '';
   return (
     <div className="item">
       <div className="item-content">
-        <a href={props.user.url} target="_blank" rel="noopener noreferrer">
-          {props.user.subType
-            ? icons[props.user.subType]
-            : icons[props.user.type]}
-          {props.user.name}
-        </a>
+        <div className="icon-title-tor">
+          <a href={props.user.url} target="_blank" rel="noopener noreferrer">
+            {props.user.subType
+              ? icons[props.user.subType]
+              : icons[props.user.type]}
+            {props.user.name}
+          </a>
+
+          {props.user.onionUrl !== undefined && (
+              <a href={props.user.onionUrl} className="onionIcon" rel="noopener noreferrer">
+                <svg viewBox="0 0 1000 500">
+                  <use href={iconSprite + "#onion"} />
+                </svg>
+              </a>
+            )}
+        </div>
         <p>{props.user.description}</p>
         <div className="separator" />
         <div className='social-media-icons'>
