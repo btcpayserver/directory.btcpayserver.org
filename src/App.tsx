ee2ff39bda700377692a174a397462b1ce84cf6e
Pavlenex <36959754+pavlenex@users.noreply.github.com>
2026-02-12 17:37:46 +0400

    remove dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import DirectoryPage from "@/pages/Directory";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="btcpay-directory-theme">
-      <TooltipProvider>
-        <DirectoryPage />
-      </TooltipProvider>
+      <DirectoryPage />
     </ThemeProvider>
   );
 }
